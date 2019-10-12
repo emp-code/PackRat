b4e9dbcc0800ea0723d6b11197104486dca5eca1
EMPcode <45713960+emp-code@users.noreply.github.com>
2019-10-12 04:43:51 +0300

    Improved simpleUint_toChar

--- packrat.c
+++ packrat.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/file.h>
 #include <stdint.h>
+#include <string.h>
 #include <math.h> // for ceil, floor
 
 #define BIT_SET(a,b) ((a) |= (1ULL<<(b)))
@@ -93,82 +94,21 @@ static int bitCheck(const char * const source, int skipBits) {
 	return BIT_CHECK(source[byteBegin], bitBegin);
 }
 
+// Store unsigned integer of 1-64 bits in target string
+static void simpleUint_toChar(char * const target, uint64_t source, const int bits) {
+	const int modulus = bits % 8;
+	const int bytes = ((bits - modulus) / 8) + ((modulus > 0) ? 1 : 0);
 
-// Store unsigned integer of 1-64 bits to a string
-static void simpleUint_toChar(char * const target, uint64_t source, const int bitCount) {
-	switch(bitCount) { // no breaks
-		case 64: if (source >= INT_2P63) {BIT_SET(target[7], 7); source -= INT_2P63;} else BIT_CLEAR(target[7], 7);
-		case 63: if (source >= INT_2P62) {BIT_SET(target[7], 6); source -= INT_2P62;} else BIT_CLEAR(target[7], 6);
-		case 62: if (source >= INT_2P61) {BIT_SET(target[7], 5); source -= INT_2P61;} else BIT_CLEAR(target[7], 5);
-		case 61: if (source >= INT_2P60) {BIT_SET(target[7], 4); source -= INT_2P60;} else BIT_CLEAR(target[7], 4);
-		case 60: if (source >= INT_2P59) {BIT_SET(target[7], 3); source -= INT_2P59;} else BIT_CLEAR(target[7], 3);
-		case 59: if (source >= INT_2P58) {BIT_SET(target[7], 2); source -= INT_2P58;} else BIT_CLEAR(target[7], 2);
-		case 58: if (source >= INT_2P57) {BIT_SET(target[7], 1); source -= INT_2P57;} else BIT_CLEAR(target[7], 1);
-		case 57: if (source >= INT_2P56) {BIT_SET(target[7], 0); source -= INT_2P56;} else BIT_CLEAR(target[7], 0);
+	if (bits < 64) {
+		source &= (UINT64_MAX >> (64 - bits));
 
-		case 56: if (source >= INT_2P55) {BIT_SET(target[6], 7); source -= INT_2P55;} else BIT_CLEAR(target[6], 7);
-		case 55: if (source >= INT_2P54) {BIT_SET(target[6], 6); source -= INT_2P54;} else BIT_CLEAR(target[6], 6);
-		case 54: if (source >= INT_2P53) {BIT_SET(target[6], 5); source -= INT_2P53;} else BIT_CLEAR(target[6], 5);
-		case 53: if (source >= INT_2P52) {BIT_SET(target[6], 4); source -= INT_2P52;} else BIT_CLEAR(target[6], 4);
-		case 52: if (source >= INT_2P51) {BIT_SET(target[6], 3); source -= INT_2P51;} else BIT_CLEAR(target[6], 3);
-		case 51: if (source >= INT_2P50) {BIT_SET(target[6], 2); source -= INT_2P50;} else BIT_CLEAR(target[6], 2);
-		case 50: if (source >= INT_2P49) {BIT_SET(target[6], 1); source -= INT_2P49;} else BIT_CLEAR(target[6], 1);
-		case 49: if (source >= INT_2P48) {BIT_SET(target[6], 0); source -= INT_2P48;} else BIT_CLEAR(target[6], 0);
-
-		case 48: if (source >= INT_2P47) {BIT_SET(target[5], 7); source -= INT_2P47;} else BIT_CLEAR(target[5], 7);
-		case 47: if (source >= INT_2P46) {BIT_SET(target[5], 6); source -= INT_2P46;} else BIT_CLEAR(target[5], 6);
-		case 46: if (source >= INT_2P45) {BIT_SET(target[5], 5); source -= INT_2P45;} else BIT_CLEAR(target[5], 5);
-		case 45: if (source >= INT_2P44) {BIT_SET(target[5], 4); source -= INT_2P44;} else BIT_CLEAR(target[5], 4);
-		case 44: if (source >= INT_2P43) {BIT_SET(target[5], 3); source -= INT_2P43;} else BIT_CLEAR(target[5], 3);
-		case 43: if (source >= INT_2P42) {BIT_SET(target[5], 2); source -= INT_2P42;} else BIT_CLEAR(target[5], 2);
-		case 42: if (source >= INT_2P41) {BIT_SET(target[5], 1); source -= INT_2P41;} else BIT_CLEAR(target[5], 1);
-		case 41: if (source >= INT_2P40) {BIT_SET(target[5], 0); source -= INT_2P40;} else BIT_CLEAR(target[5], 0);
-
-		case 40: if (source >= INT_2P39) {BIT_SET(target[4], 7); source -= INT_2P39;} else BIT_CLEAR(target[4], 7);
-		case 39: if (source >= INT_2P38) {BIT_SET(target[4], 6); source -= INT_2P38;} else BIT_CLEAR(target[4], 6);
-		case 38: if (source >= INT_2P37) {BIT_SET(target[4], 5); source -= INT_2P37;} else BIT_CLEAR(target[4], 5);
-		case 37: if (source >= INT_2P36) {BIT_SET(target[4], 4); source -= INT_2P36;} else BIT_CLEAR(target[4], 4);
-		case 36: if (source >= INT_2P35) {BIT_SET(target[4], 3); source -= INT_2P35;} else BIT_CLEAR(target[4], 3);
-		case 35: if (source >= INT_2P34) {BIT_SET(target[4], 2); source -= INT_2P34;} else BIT_CLEAR(target[4], 2);
-		case 34: if (source >= INT_2P33) {BIT_SET(target[4], 1); source -= INT_2P33;} else BIT_CLEAR(target[4], 1);
-		case 33: if (source >= INT_2P32) {BIT_SET(target[4], 0); source -= INT_2P32;} else BIT_CLEAR(target[4], 0);
-
-		case 32: if (source >= INT_2P31) {BIT_SET(target[3], 7); source -= INT_2P31;} else BIT_CLEAR(target[3], 7);
-		case 31: if (source >= INT_2P30) {BIT_SET(target[3], 6); source -= INT_2P30;} else BIT_CLEAR(target[3], 6);
-		case 30: if (source >= INT_2P29) {BIT_SET(target[3], 5); source -= INT_2P29;} else BIT_CLEAR(target[3], 5);
-		case 29: if (source >= INT_2P28) {BIT_SET(target[3], 4); source -= INT_2P28;} else BIT_CLEAR(target[3], 4);
-		case 28: if (source >= INT_2P27) {BIT_SET(target[3], 3); source -= INT_2P27;} else BIT_CLEAR(target[3], 3);
-		case 27: if (source >= INT_2P26) {BIT_SET(target[3], 2); source -= INT_2P26;} else BIT_CLEAR(target[3], 2);
-		case 26: if (source >= INT_2P25) {BIT_SET(target[3], 1); source -= INT_2P25;} else BIT_CLEAR(target[3], 1);
-		case 25: if (source >= INT_2P24) {BIT_SET(target[3], 0); source -= INT_2P24;} else BIT_CLEAR(target[3], 0);
-
-		case 24: if (source >= INT_2P23) {BIT_SET(target[2], 7); source -= INT_2P23;} else BIT_CLEAR(target[2], 7);
-		case 23: if (source >= INT_2P22) {BIT_SET(target[2], 6); source -= INT_2P22;} else BIT_CLEAR(target[2], 6);
-		case 22: if (source >= INT_2P21) {BIT_SET(target[2], 5); source -= INT_2P21;} else BIT_CLEAR(target[2], 5);
-		case 21: if (source >= INT_2P20) {BIT_SET(target[2], 4); source -= INT_2P20;} else BIT_CLEAR(target[2], 4);
-		case 20: if (source >= INT_2P19) {BIT_SET(target[2], 3); source -= INT_2P19;} else BIT_CLEAR(target[2], 3);
-		case 19: if (source >= INT_2P18) {BIT_SET(target[2], 2); source -= INT_2P18;} else BIT_CLEAR(target[2], 2);
-		case 18: if (source >= INT_2P17) {BIT_SET(target[2], 1); source -= INT_2P17;} else BIT_CLEAR(target[2], 1);
-		case 17: if (source >= INT_2P16) {BIT_SET(target[2], 0); source -= INT_2P16;} else BIT_CLEAR(target[2], 0);
-
-		case 16: if (source >= INT_2P15) {BIT_SET(target[1], 7); source -= INT_2P15;} else BIT_CLEAR(target[1], 7);
-		case 15: if (source >= INT_2P14) {BIT_SET(target[1], 6); source -= INT_2P14;} else BIT_CLEAR(target[1], 6);
-		case 14: if (source >= INT_2P13) {BIT_SET(target[1], 5); source -= INT_2P13;} else BIT_CLEAR(target[1], 5);
-		case 13: if (source >= INT_2P12) {BIT_SET(target[1], 4); source -= INT_2P12;} else BIT_CLEAR(target[1], 4);
-		case 12: if (source >= INT_2P11) {BIT_SET(target[1], 3); source -= INT_2P11;} else BIT_CLEAR(target[1], 3);
-		case 11: if (source >= INT_2P10) {BIT_SET(target[1], 2); source -= INT_2P10;} else BIT_CLEAR(target[1], 2);
-		case 10: if (source >= INT_2P9)  {BIT_SET(target[1], 1); source -= INT_2P9;}  else BIT_CLEAR(target[1], 1);
-		case  9: if (source >= INT_2P8)  {BIT_SET(target[1], 0); source -= INT_2P8;}  else BIT_CLEAR(target[1], 0);
-
-		case  8: if (source >= INT_2P7)  {BIT_SET(target[0], 7); source -= INT_2P7;} else BIT_CLEAR(target[0], 7);
-		case  7: if (source >= INT_2P6)  {BIT_SET(target[0], 6); source -= INT_2P6;} else BIT_CLEAR(target[0], 6);
-		case  6: if (source >= INT_2P5)  {BIT_SET(target[0], 5); source -= INT_2P5;} else BIT_CLEAR(target[0], 5);
-		case  5: if (source >= INT_2P4)  {BIT_SET(target[0], 4); source -= INT_2P4;} else BIT_CLEAR(target[0], 4);
-		case  4: if (source >= INT_2P3)  {BIT_SET(target[0], 3); source -= INT_2P3;} else BIT_CLEAR(target[0], 3);
-		case  3: if (source >= INT_2P2)  {BIT_SET(target[0], 2); source -= INT_2P2;} else BIT_CLEAR(target[0], 2);
-		case  2: if (source >= INT_2P1)  {BIT_SET(target[0], 1); source -= INT_2P1;} else BIT_CLEAR(target[0], 1);
-		case  1: if (source >= INT_2P0)  {BIT_SET(target[0], 0); source -= INT_2P0;} else BIT_CLEAR(target[0], 0);
+		uint64_t orig = 0;
+		memcpy(&orig, target, bytes);
+		orig &= (UINT64_MAX << bits); // zero bits we'll copy over from the source (non-preserved bits)
+		source |= orig; // add preserved bits
 	}
+
+	memcpy(target, &source, bytes);
 }
 
 // Get unsigned integer of 1-64 bits from a char array of 1-8 bytes
