064a6186f9782b66450e769f8a2d9462e00a0ca7
emp-code <dev@empost.eu>
2024-02-13 23:04:53 +0200

    Use one bit of ID in header to indicate whether file is PRI or PRD

--- packrat.c
+++ packrat.c
@@ -7,17 +7,24 @@
 #include "packrat.h"
 
 // Pack Rat v3 - File signature 'PR'
-#define PACKRAT_HEADER_SIG 0x5250
 #define PACKRAT_OFFSET_BITSPOS 16
 #define PACKRAT_OFFSET_BITSLEN 1
-struct packrat_header {
-	uint64_t pr: 16;
-	uint64_t id: 32;
 
-	uint64_t bitsPos: 5; // 0-31 -> 16-47. 64 KiB - 128 TiB for total data
-	uint64_t bitsLen: 5; // 0-31 -> 1-32. 4 B - 4 GiB per file (Zero variant only)
-	uint64_t unused: 5;
-	uint64_t allowMod: 1; // If 0, the library refuses to modify existing data.
+#define PACKRAT_HEADER_SIG 0x5250
+#define PACKRAT_HEADER_LEN 8
+union packrat_header {
+	uint64_t u64;
+	unsigned char chr[8];
+
+	struct {
+		uint64_t pr: 16;
+		uint64_t id: 32;
+
+		uint64_t bitsPos: 5; // 0-31 -> 16-47. 64 KiB - 128 TiB for total data
+		uint64_t bitsLen: 5; // 0-31 -> 1-32. 4 B - 4 GiB per file (Zero variant only)
+		uint64_t unused: 5;
+		uint64_t allowMod: 1; // If 0, the library refuses to modify existing data.
+	} fields;
 };
 
 static int div_floor(const int a, const int b) {
@@ -59,14 +66,17 @@ static uint64_t extractPruint(unsigned char * const src, const int startBit, con
 	return r.u64;
 }
 
-static bool headersMatch(const struct packrat_header * const h1, const int fd) {
-	unsigned char h2[sizeof(struct packrat_header)];
-	if (read(fd, h2, sizeof(struct packrat_header)) != sizeof(struct packrat_header)) return false;
-	return (memcmp(h1, h2, sizeof(struct packrat_header)) == 0) ? true : false;
+static bool headersMatch(const union packrat_header pri_hdr, const int fd) {
+	union packrat_header prd_hdr;
+	if (read(fd, prd_hdr.chr, PACKRAT_HEADER_LEN) != PACKRAT_HEADER_LEN) return false;
+
+	if ((pri_hdr.fields.id & 1) != 1 && (prd_hdr.fields.id & 1) != 0) return false;
+	prd_hdr.fields.id |= 1;
+	return (pri_hdr.u64 == prd_hdr.u64) ? true : false;
 }
 
 static int pr_get_zero(const int pri, const int prd, const unsigned long fileNum, const int bitsLen, const int bitsPos, unsigned char ** const data) {
-	const int64_t priPos = sizeof(struct packrat_header) + div_floor((bitsLen + bitsPos) * fileNum, 8); // Byte number to start reading from
+	const int64_t priPos = PACKRAT_HEADER_LEN + div_floor((bitsLen + bitsPos) * fileNum, 8); // Byte number to start reading from
 	const long skipBits = ((bitsLen + bitsPos) * fileNum) % 8; // Number of irrelevant bits in the first byte, to be skipped
 	const long priBytes = div_ceil(skipBits + bitsLen + bitsPos, 8);
 
@@ -82,7 +92,7 @@ static int pr_get_zero(const int pri, const int prd, const unsigned long fileNum
 	*data = malloc(readLen);
 	if (*data == NULL) return PACKRAT_ERROR_ALLOC;
 
-	r = pread(prd, *data, readLen, sizeof(struct packrat_header) + readPos);
+	r = pread(prd, *data, readLen, PACKRAT_HEADER_LEN + readPos);
 	if (r != (ssize_t)readLen) {
 		free(*data);
 		*data = NULL;
@@ -93,7 +103,7 @@ static int pr_get_zero(const int pri, const int prd, const unsigned long fileNum
 }
 
 static int pr_get_compact(const int pri, const int prd, const unsigned long fileNum, const int bitsPos, unsigned char ** const data) {
-	const int64_t priPos = sizeof(struct packrat_header) + div_floor(bitsPos * fileNum, 8); // Byte number to start reading from
+	const int64_t priPos = PACKRAT_HEADER_LEN + div_floor(bitsPos * fileNum, 8); // Byte number to start reading from
 	const long skipBits = (bitsPos * fileNum) % 8; // Number of irrelevant bits in the first byte, to be skipped
 	const long priBytes = div_ceil(skipBits + (bitsPos * 2), 8);
 
@@ -111,7 +121,7 @@ static int pr_get_compact(const int pri, const int prd, const unsigned long file
 	} else if (r == div_ceil(skipBits + bitsPos, 8)) {
 		const off_t prdSize = lseek(prd, 0, SEEK_END);
 		if (prdSize < 0) return PACKRAT_ERROR_READ_PRI;
-		readLen = (prdSize - sizeof(struct packrat_header)) - readPos;
+		readLen = (prdSize - PACKRAT_HEADER_LEN) - readPos;
 	} else {
 		return PACKRAT_ERROR_READ_PRI;
 	}
@@ -121,7 +131,7 @@ static int pr_get_compact(const int pri, const int prd, const unsigned long file
 	*data = malloc(readLen);
 	if (*data == NULL) return PACKRAT_ERROR_ALLOC;
 
-	r = pread(prd, *data, readLen, sizeof(struct packrat_header) + readPos);
+	r = pread(prd, *data, readLen, PACKRAT_HEADER_LEN + readPos);
 	if (r != (ssize_t)readLen) {
 		free(*data);
 		*data = NULL;
@@ -142,11 +152,11 @@ int packrat_get(const char * const pathPri, const char * const pathPrd, const un
 	if (prd < 0) return PACKRAT_ERROR_OPEN;
 	if (flock(prd, LOCK_EX | LOCK_NB) != 0) {close(pri); close(prd); return PACKRAT_ERROR_LOCK;}
 
-	struct packrat_header header;
-	if (read(pri, (unsigned char*)&header, sizeof(struct packrat_header)) != sizeof(struct packrat_header)) {close(prd); close(pri); return PACKRAT_ERROR_READ_PRI;}
-	if (!headersMatch(&header, prd)) return PACKRAT_ERROR_MISMATCH;
+	union packrat_header header;
+	if (read(pri, header.chr, PACKRAT_HEADER_LEN) != PACKRAT_HEADER_LEN) {close(prd); close(pri); return PACKRAT_ERROR_READ_PRI;}
+	if (!headersMatch(header, prd)) return PACKRAT_ERROR_MISMATCH;
 
-	const int ret = (header.bitsLen == 0) ? pr_get_compact(pri, prd, fileNum, header.bitsPos + PACKRAT_OFFSET_BITSPOS, data) : pr_get_zero(pri, prd, fileNum, header.bitsLen + PACKRAT_OFFSET_BITSLEN, header.bitsPos + PACKRAT_OFFSET_BITSPOS, data);
+	const int ret = (header.fields.bitsLen == 0) ? pr_get_compact(pri, prd, fileNum, header.fields.bitsPos + PACKRAT_OFFSET_BITSPOS, data) : pr_get_zero(pri, prd, fileNum, header.fields.bitsLen + PACKRAT_OFFSET_BITSLEN, header.fields.bitsPos + PACKRAT_OFFSET_BITSPOS, data);
 
 	flock(pri, LOCK_UN);
 	close(pri);
@@ -160,13 +170,13 @@ static int pr_add_zero(const int pri, const int prd, int bitsLen, int bitsPos, c
 	if (lenData >= (1L << bitsLen)) return PACKRAT_ERROR_TOOBIG;
 
 	const off_t prdSize = lseek(prd, 0, SEEK_END);
-	if (prdSize < (off_t)sizeof(struct packrat_header)) return PACKRAT_ERROR_READ_PRD;
-	if (prdSize - sizeof(struct packrat_header) + lenData >= (1UL << bitsPos)) return PACKRAT_ERROR_TOOBIG;
+	if (prdSize < PACKRAT_HEADER_LEN) return PACKRAT_ERROR_READ_PRD;
+	if (prdSize - PACKRAT_HEADER_LEN + lenData >= (1L << bitsPos)) return PACKRAT_ERROR_TOOBIG;
 
 	const off_t priSize = lseek(pri, 0, SEEK_END);
-	if (priSize < (off_t)sizeof(struct packrat_header)) return PACKRAT_ERROR_READ_PRI;
+	if (priSize < PACKRAT_HEADER_LEN) return PACKRAT_ERROR_READ_PRI;
 
-	const int entries = div_floor((priSize - sizeof(struct packrat_header)) * 8, bitsLen + bitsPos);
+	const int entries = div_floor((priSize - PACKRAT_HEADER_LEN) * 8, bitsLen + bitsPos);
 	const int usedBits = ((bitsLen + bitsPos) * entries) % 8; // Number of currently-used bits in the last byte
 
 	const int lenTbw = div_ceil(usedBits + bitsLen + bitsPos, 8);
@@ -178,7 +188,7 @@ static int pr_add_zero(const int pri, const int prd, int bitsLen, int bitsPos, c
 	// Zero variant stores the length and position of each file
 	const uint64_t raw1 = lenData;
 	appendPruint(tbw, usedBits, raw1, bitsLen);
-	const uint64_t raw2 = prdSize - sizeof(struct packrat_header);
+	const uint64_t raw2 = prdSize - PACKRAT_HEADER_LEN;
 	appendPruint(tbw, usedBits + bitsLen, raw2, bitsPos);
 
 	if (pwrite(pri, (unsigned char*)&tbw, lenTbw, (usedBits > 0) ? priSize - 1 : priSize) != lenTbw) return PACKRAT_ERROR_WRITE;
@@ -191,13 +201,13 @@ static int pr_add_compact(const int pri, const int prd, int bitsPos, const unsig
 	if (bitsPos > 47) return PACKRAT_ERROR_HEADER;
 
 	const off_t prdSize = lseek(prd, 0, SEEK_END);
-	if (prdSize < (off_t)sizeof(struct packrat_header)) return PACKRAT_ERROR_READ_PRD;
-	if (prdSize - sizeof(struct packrat_header) + lenData >= (1UL << bitsPos)) return PACKRAT_ERROR_TOOBIG;
+	if (prdSize < PACKRAT_HEADER_LEN) return PACKRAT_ERROR_READ_PRD;
+	if (prdSize - PACKRAT_HEADER_LEN + lenData >= (1L << bitsPos)) return PACKRAT_ERROR_TOOBIG;
 
 	const off_t priSize = lseek(pri, 0, SEEK_END);
-	if (priSize < (off_t)sizeof(struct packrat_header)) return PACKRAT_ERROR_READ_PRI;
+	if (priSize < PACKRAT_HEADER_LEN) return PACKRAT_ERROR_READ_PRI;
 
-	const int entries = div_floor((priSize - sizeof(struct packrat_header)) * 8, bitsPos);
+	const int entries = div_floor((priSize - PACKRAT_HEADER_LEN) * 8, bitsPos);
 	const int usedBits = (bitsPos * entries) % 8; // Number of currently-used bits in the last byte
 
 	const int lenTbw = div_ceil(usedBits + bitsPos, 8);
@@ -207,7 +217,7 @@ static int pr_add_compact(const int pri, const int prd, int bitsPos, const unsig
 	if (usedBits > 0 && pread(pri, tbw, 1, priSize - 1) != 1) return PACKRAT_ERROR_READ_PRI;
 
 	// Compact variant stores the position (starting byte) of each file
-	const uint64_t raw = prdSize - sizeof(struct packrat_header);
+	const uint64_t raw = prdSize - PACKRAT_HEADER_LEN;
 	appendPruint(tbw, usedBits, raw, bitsPos);
 
 	if (pwrite(pri, (unsigned char*)&tbw, lenTbw, (usedBits > 0) ? priSize - 1 : priSize) != lenTbw) return PACKRAT_ERROR_WRITE;
@@ -227,11 +237,11 @@ int packrat_add(const char * const pathPri, const char * const pathPrd, const un
 	if (prd < 0) return PACKRAT_ERROR_OPEN;
 	if (flock(prd, LOCK_EX | LOCK_NB) != 0) {close(pri); close(prd); return PACKRAT_ERROR_LOCK;}
 
-	struct packrat_header header;
-	if (read(pri, (unsigned char*)&header, sizeof(struct packrat_header)) != sizeof(struct packrat_header)) {close(prd); close(pri); return PACKRAT_ERROR_READ_PRI;}
-	if (!headersMatch(&header, prd)) return PACKRAT_ERROR_MISMATCH;
+	union packrat_header header;
+	if (read(pri, header.chr, PACKRAT_HEADER_LEN) != PACKRAT_HEADER_LEN) {close(prd); close(pri); return PACKRAT_ERROR_READ_PRI;}
+	if (!headersMatch(header, prd)) return PACKRAT_ERROR_MISMATCH;
 
-	const int ret = (header.bitsLen == 0) ? pr_add_compact(pri, prd, header.bitsPos + PACKRAT_OFFSET_BITSPOS, data, lenData) : pr_add_zero(pri, prd, header.bitsLen + PACKRAT_OFFSET_BITSLEN, header.bitsPos + PACKRAT_OFFSET_BITSPOS, data, lenData);
+	const int ret = (header.fields.bitsLen == 0) ? pr_add_compact(pri, prd, header.fields.bitsPos + PACKRAT_OFFSET_BITSPOS, data, lenData) : pr_add_zero(pri, prd, header.fields.bitsLen + PACKRAT_OFFSET_BITSLEN, header.fields.bitsPos + PACKRAT_OFFSET_BITSPOS, data, lenData);
 
 	flock(pri, LOCK_UN);
 	close(pri);
@@ -252,15 +262,22 @@ int packrat_create(const char * const pathPri, const char * const pathPrd, const
 	if (pri < 0) return PACKRAT_ERROR_OPEN;
 	if (flock(pri, LOCK_EX | LOCK_NB) != 0) {close(pri); return PACKRAT_ERROR_LOCK;}
 
-	struct packrat_header header;
-	header.pr = PACKRAT_HEADER_SIG;
-	header.id = arc4random();
-	header.bitsPos = bitsPos - PACKRAT_OFFSET_BITSPOS;
-	header.bitsLen = (bitsLen == 0) ? 0 : bitsLen - PACKRAT_OFFSET_BITSLEN;
-	header.unused = 0;
-	header.allowMod = 1;
+	union packrat_header header;
+	header.fields.pr = PACKRAT_HEADER_SIG;
+	header.fields.id = arc4random() | 1; // PRI: id & 1 == 1
+	header.fields.bitsPos = bitsPos - PACKRAT_OFFSET_BITSPOS;
+	header.fields.bitsLen = (bitsLen == 0) ? 0 : bitsLen - PACKRAT_OFFSET_BITSLEN;
+	header.fields.unused = 0;
+	header.fields.allowMod = 1;
 
-	const bool ok = (write(prd, (unsigned char*)&header, sizeof(struct packrat_header)) == sizeof(struct packrat_header) && write(pri, (unsigned char*)&header, sizeof(struct packrat_header)) == sizeof(struct packrat_header));
+	bool ok = false;
+	if (write(pri, header.chr, PACKRAT_HEADER_LEN) == PACKRAT_HEADER_LEN) {
+		header.fields.id ^= 1; // PRD: id & 1 == 0
+
+		if (write(prd, header.chr, PACKRAT_HEADER_LEN) == PACKRAT_HEADER_LEN) {
+			ok = true;
+		}
+	}
 
 	flock(pri, LOCK_UN);
 	close(pri);
