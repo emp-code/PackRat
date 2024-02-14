5da3a8897ae47894724a15a5c8617b84851ee819
emp-code <dev@empost.eu>
2024-02-14 17:56:21 +0200

    Clean up and modernize Analyzer

--- Utils/Analyzer.c
+++ Utils/Analyzer.c
@@ -7,17 +7,24 @@
 #include <unistd.h>
 
 // Pack Rat v3 - File signature 'PR'
-#define PACKRAT_HEADER_SIG 0x5250
 #define PACKRAT_OFFSET_BITSPOS 16
 #define PACKRAT_OFFSET_BITSLEN 1
-struct packrat_header {
-	uint64_t pr: 16;
-	uint64_t id: 32;
 
-	uint64_t bitsPos: 5; // 0-31 -> 16-47. 64 KiB - 128 TiB for total data
-	uint64_t bitsLen: 5; // 0-31 -> 1-32. 4 B - 4 GiB per file (Zero variant only)
-	uint64_t unused: 5;
-	uint64_t allowMod: 1; // If 0, the library refuses to modify existing data.
+#define PACKRAT_HEADER_SIG 0x5250
+#define PACKRAT_HEADER_LEN 8
+union packrat_header {
+	uint64_t u64;
+	unsigned char chr[8];
+
+	struct {
+		uint64_t pr: 16;
+		uint64_t id: 32;
+
+		uint64_t bitsPos: 5; // 0-31 -> 16-47. 64 KiB - 128 TiB for total data
+		uint64_t bitsLen: 5; // 0-31 -> 1-32. 4 B - 4 GiB per file (Zero variant only)
+		uint64_t unused: 5;
+		uint64_t allowMod: 1; // If 0, the library refuses to modify existing data.
+	} fields;
 };
 
 static int div_floor(const int a, const int b) {
@@ -77,26 +84,30 @@ int main(int argc, char *argv[]) {
 
 	int pri = open(argv[1], O_RDONLY);
 	if (pri == -1) {
-		perror("Failed to open PRI file");
+		perror("Failed to open PRI file.");
 		return 1;
 	}
 
-	struct packrat_header header;
-	ssize_t bytesRead = read(pri, (unsigned char*)&header, sizeof(struct packrat_header));
+	union packrat_header header;
+	ssize_t bytesRead = read(pri, header.chr, PACKRAT_HEADER_LEN);
 	if (bytesRead == -1) {
-		perror("Failed reading PRI file");
-	} else if (bytesRead != sizeof(struct packrat_header)) {
+		perror("Failed reading PRI file.");
+		close(pri);
+		return 1;
+	} else if (bytesRead != PACKRAT_HEADER_LEN) {
 		puts("Invalid PRI file format.");
+		close(pri);
 		return 1;
 	}
 
-	if (header.pr != PACKRAT_HEADER_SIG) {
+	if (header.fields.pr != PACKRAT_HEADER_SIG) {
 		puts("Invalid file signature.");
+		close(pri);
 		return 1;
 	}
 
-	const int bitsPos = header.bitsPos + PACKRAT_OFFSET_BITSPOS;
-	const int bitsLen = (header.bitsLen == 0) ? 0 : header.bitsLen + PACKRAT_OFFSET_BITSLEN;
+	const int bitsPos = header.fields.bitsPos + PACKRAT_OFFSET_BITSPOS;
+	const int bitsLen = (header.fields.bitsLen == 0) ? 0 : header.fields.bitsLen + PACKRAT_OFFSET_BITSLEN;
 
 	if (bitsLen == 0) {
 		puts("Variant: Compact");
@@ -108,8 +119,9 @@ int main(int argc, char *argv[]) {
 		printf("Total bits per entry: %d\n", bitsPos + bitsLen);
 	}
 
-	if (header.bitsPos == 0) {
+	if (header.fields.bitsPos == 0) {
 		puts("Invalid bitsPos.");
+		close(pri);
 		return 1;
 	}
 
@@ -117,15 +129,18 @@ int main(int argc, char *argv[]) {
 
 	off_t indexSize = lseek(pri, 0, SEEK_END);
 	if (indexSize == -1) {
-		perror("Failed lseek()");
+		perror("Failed getting size of PRI file.");
+		close(pri);
+		return 1;
 	}
-	indexSize -= sizeof(struct packrat_header);
+	indexSize -= PACKRAT_HEADER_LEN;
 
 	const int entryCount = div_floor(indexSize * 8, bitsPos + bitsLen);
 	printf("Number of entries: %d\n", entryCount);
 
 	if (entryCount == 0) {
 		puts("Empty archive.");
+		close(pri);
 		return 0;
 	}
 
@@ -134,15 +149,19 @@ int main(int argc, char *argv[]) {
 
 	unsigned char * const raw = malloc(indexSize);
 	if (raw == NULL) {
-		perror("Failed malloc()");
+		perror("Failed allocation.");
+		close(pri);
 		return 1;
 	}
 
-	if (pread(pri, raw, indexSize, sizeof(struct packrat_header)) != (ssize_t)indexSize) {
+	if (pread(pri, raw, indexSize, PACKRAT_HEADER_LEN) != (ssize_t)indexSize) {
 		puts("Failed reading PRI file.");
 		free(raw);
+		close(pri);
 		return 1;
 	}
+
+	close(pri);
 
 	if (bitsLen == 0) {
 		int posNow = 0;
@@ -171,7 +190,6 @@ int main(int argc, char *argv[]) {
 	}
 
 	free(raw);
-	close(pri);
 	puts("");
 
 	if (argc < 3) {
