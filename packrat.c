7977b64cd1f13b211e5d7c9bd64a1d16cc87356e
emp-code <dev@empost.eu>
2024-02-15 18:28:23 +0200

    Use 64-bit integers to support very large files

--- packrat.c
+++ packrat.c
@@ -27,11 +27,11 @@ union packrat_header {
 	} fields;
 };
 
-static int div_floor(const int a, const int b) {
+static int div_floor(const int64_t a, const int64_t b) {
 	return (a - (a % b)) / b;
 }
 
-static int div_ceil(const int a, const int b) {
+static int div_ceil(const int64_t a, const int64_t b) {
 	return (a % b == 0) ? a / b : div_floor(a, b) + 1;
 }
 
